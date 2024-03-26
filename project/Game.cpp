c2d2c1044508068c84e5bc13fc2e61708e8c2686
Julia Pabst <ju.pabst@gmail.com>
2024-03-26 17:55:02 +0100

    implement fillField() function in Game class

--- project/Game.cpp
+++ project/Game.cpp
@@ -1,7 +1,20 @@
 #include "Game.h"
+#include <random>
+#include <cstdlib> // srand()
+#include <ctime>
+#include <iostream>
 
 Game::Game() : x_(5), y_(5), z_(10), field_(x_, vector<vector<int>>(y_, vector<int>(z_, 0))){
-    fillGame();
+    fillField();
 }
 
-void Game::fillField() {}
+void Game::fillField() {
+    srand(time(NULL));
+    for(int i = 0; i < x_; i++){
+        for(int j = 0; j < y_; j++){
+            for(int k = 0; k < z_; k++){
+                field_[i][j][k] = (rand() % 9) + 1;
+            }
+        }
+    }
+}
