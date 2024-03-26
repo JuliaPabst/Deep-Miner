c53cec7abc1d36a01899068496c5b444611afeaf
Julia Pabst <ju.pabst@gmail.com>
2024-03-26 16:54:43 +0100

    add structure to Bot.h

--- project/Bot.h
+++ project/Bot.h
@@ -1,9 +1,25 @@
 #ifndef PROJECT_BOT_H
 #define PROJECT_BOT_H
+#include <iostream>
+#include "Game.h"
 
+using namespace std;
+
+typedef enum BotType{
+    sort, dig
+} BotType;
 
 class Bot {
-
+public:
+    Bot();
+    virtual ~Bot();
+    virtual void act(Game game) = 0;
+    void place();
+    void move();
+private:
+    BotType botType;
+    vector <int> position;
+    int points;
 };
 
 
