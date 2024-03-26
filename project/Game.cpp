dcd2080f604e0e5379f4ee9963df08cf1f6c6850
Julia Pabst <ju.pabst@gmail.com>
2024-03-26 17:44:24 +0100

    add structure to Game.h and Game.cpp files

--- project/Game.cpp
+++ project/Game.cpp
@@ -1,5 +1,7 @@
-//
-// Created by Julia Pabst on 26.03.24.
-//
-
 #include "Game.h"
+
+Game::Game() : x_(5), y_(5), z_(10), field_(x_, vector<vector<int>>(y_, vector<int>(z_, 0))){
+    fillGame();
+}
+
+void Game::fillField() {}
--- project/Game.h
+++ project/Game.h
@@ -1,9 +1,20 @@
 #ifndef PROJECT_GAME_H
 #define PROJECT_GAME_H
+#include <iostream>
 
+
+using namespace std;
 
 class Game {
+public:
+    Game();
+    void fillField();
 
+private:
+    int x_;
+    int y_;
+    int z_;
+    vector<vector<vector<int>>> field_;
 };
 
 
